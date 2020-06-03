b675432bd308f2f520758538ed8a64bb0b4da77a
Denis W. Thürling <denisthuerling@outlook.de>
2020-06-03 19:21:14 +0200

    Updates

---
+++ precache-manifest.b7c17d7c99f08ba19f6cbeed4d4109c4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df2db15222e84149cc1f7871cb5a65bd",
+    "url": "/challenges/index.html"
+  },
+  {
+    "revision": "e5854ecf3b70e9591376",
+    "url": "/challenges/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8c004678f5dbc751d7bd",
+    "url": "/challenges/static/js/2.f8c43ac1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/challenges/static/js/2.f8c43ac1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5854ecf3b70e9591376",
+    "url": "/challenges/static/js/main.46b6547b.chunk.js"
+  },
+  {
+    "revision": "7fec57868f952947a5c5",
+    "url": "/challenges/static/js/runtime-main.d9870398.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/challenges/static/media/logo.5d5d9eef.svg"
+  }
+]);
